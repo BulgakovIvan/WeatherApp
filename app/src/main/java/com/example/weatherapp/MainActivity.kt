3f6890d5ac69fdafb051d7ed5f43d5e8c4b4ed52
BulgakovIvan <bulgivan@mail.ru>
2021-06-18 17:41:35 +0300

    Setup UI

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -16,6 +16,7 @@ import android.provider.Settings
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import com.example.weatherapp.databinding.ActivityMainBinding
 import com.example.weatherapp.models.WeatherResponse
 import com.example.weatherapp.network.WeatherService
 import com.google.android.gms.location.*
@@ -29,8 +30,11 @@ import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bi: ActivityMainBinding
     val TAG = "ups"
     private lateinit var mFusedLocationClient: FusedLocationProviderClient
 
@@ -38,7 +42,10 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bi = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bi.root)
+
+        showCustomProgressDialog()
 
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
@@ -119,10 +126,10 @@ class MainActivity : AppCompatActivity() {
         override fun onLocationResult(locationResult: LocationResult) {
             val mLastLocation: Location = locationResult.lastLocation
             val latitude = mLastLocation.latitude
-            Log.e(TAG, "Current Latitude: $latitude")
+//            Log.e(TAG, "Current Latitude: $latitude")
 
             val longitude = mLastLocation.longitude
-            Log.e(TAG, "Current Longitude: $longitude")
+//            Log.e(TAG, "Current Longitude: $longitude")
 
             getLocationWeatherDetails(latitude, longitude)
         }
@@ -143,14 +150,16 @@ class MainActivity : AppCompatActivity() {
                 latitude, longitude, Constants.METRIC_UNIT, getString(R.string.APP_ID)
             )
 
-            showCustomProgressDialog()
-
             listCall.enqueue(object : Callback<WeatherResponse> {
                 override fun onResponse(call: Call<WeatherResponse>,
                                         response: Response<WeatherResponse>) {
 
                     if (response.isSuccessful) {
                         val weatherList: WeatherResponse? = response.body()
+
+                        if (weatherList != null) {
+                            setupUI(weatherList)
+                        }
 
                         Log.e(TAG, weatherList.toString())
 
@@ -184,5 +193,56 @@ class MainActivity : AppCompatActivity() {
         if (mProgressDialog != null) {
             mProgressDialog!!.dismiss()
         }
+    }
+
+    private fun setupUI(weatherList: WeatherResponse) {
+        for (i in weatherList.weather.indices) {
+
+            bi.tvMain.text = weatherList.weather[i].main
+            bi.tvMainDescription.text = weatherList.weather[i].description
+            bi.tvTemp.text = getString(R.string.temp, weatherList.main.temp.toString(), getUnit())
+            bi.tvHumidity.text = getString(R.string.humidity, weatherList.main.humidity.toString(), " %")
+            bi.tvMin.text = getString(R.string.min, weatherList.main.temp_min.toString())
+            bi.tvMax.text = getString(R.string.max, weatherList.main.temp_max.toString())
+            bi.tvSpeed.text = weatherList.wind.speed.toString()
+            bi.tvName.text = weatherList.name
+            bi.tvCountry.text = weatherList.sys.country
+            bi.tvSunriseTime.text = unixTime(weatherList.sys.sunrise)
+            bi.tvSunsetTime.text = unixTime(weatherList.sys.sunset)
+
+            when (weatherList.weather[i].icon) {
+                "01d" -> bi.ivMain.setImageResource(R.drawable.sunny)
+                "02d" -> bi.ivMain.setImageResource(R.drawable.cloud)
+                "03d" -> bi.ivMain.setImageResource(R.drawable.cloud)
+                "04d" -> bi.ivMain.setImageResource(R.drawable.cloud)
+                "04n" -> bi.ivMain.setImageResource(R.drawable.cloud)
+                "10d" -> bi.ivMain.setImageResource(R.drawable.rain)
+                "11d" -> bi.ivMain.setImageResource(R.drawable.storm)
+                "13d" -> bi.ivMain.setImageResource(R.drawable.snowflake)
+                "01n" -> bi.ivMain.setImageResource(R.drawable.cloud)
+                "02n" -> bi.ivMain.setImageResource(R.drawable.cloud)
+                "03n" -> bi.ivMain.setImageResource(R.drawable.cloud)
+                "10n" -> bi.ivMain.setImageResource(R.drawable.cloud)
+                "11n" -> bi.ivMain.setImageResource(R.drawable.rain)
+                "13n" -> bi.ivMain.setImageResource(R.drawable.snowflake)
+            }
+        }
+    }
+
+    private fun getUnit(): String {
+        val country = Locale.getDefault().country
+
+        return if ("US" == country || "LR" == country || "MM" == country) {
+            "°F"
+        } else {
+            "°C"
+        }
+    }
+
+    private fun unixTime(timex: Long): String? {
+        val date = Date(timex * 1000L)
+        val sdf = SimpleDateFormat("HH:mm", Locale.getDefault())
+        sdf.timeZone = TimeZone.getDefault()
+        return sdf.format(date)
     }
 }
